bb6a50d2e685d0d3987f59f708b363c4f2c27ae0
Divey Airen <diveyairen@microsoft.com>
2024-12-23 16:04:16 +0530

    Revert "Merge pull request #1 from Prashant-Sahu/ft-divey"
    This reverts commit 7d9c69f51520c1bdfa17796350cf3afad5a9d0b6, reversing
    changes made to 0e17622265673478499a04934e59d6c6c5eb2d47.

--- src/main/java/com/sahuexports/sahuexportsbackend/repository/user/UserRepositoryInterface.java
+++
@@ -1,8 +0,0 @@
-package com.sahuexports.sahuexportsbackend.repository.user;
-
-import com.sahuexports.sahuexportsbackend.model.User;
-
-public interface UserRepositoryInterface {
-    void save(User user);
-    User findByUserName(String userName);
-}
